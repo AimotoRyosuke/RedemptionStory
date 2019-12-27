578f8c9528d3b15c04f00567702ecd08f9ef76e4
AimotoRyosuke <r.aimoto97vg@gmail.com>
2019-12-27 21:50:32 +0900

    setting basic_auth & image_uploader

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: proc{Rails.env.production?}
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
   def  configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :sex])
   end
+
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic_auth[:username] && password == Rails.application.credentials.basic_auth[:password]
+    end
+  end
+
 end
